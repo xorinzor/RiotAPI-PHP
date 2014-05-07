1ce84bced9107ce46b57f3028c794dcf63bfa892
Jorin Vermeulen <jorin.vermeulen@gmail.com>
2014-05-07 15:51:15 +0200

    Update install-apcu.sh

--- tests/bin/install-apcu.sh
+++ tests/bin/install-apcu.sh
@@ -11,7 +11,7 @@ fi
 sudo apt-get install autoconf
  
 # install this version
-APCU=4.0.2
+APCU=4.0.4
  
 # compile manually, because `pecl install apcu-beta` keep asking questions
 wget http://pecl.php.net/get/apcu-$APCU.tgz
